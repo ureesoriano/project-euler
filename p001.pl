95e96acb651a087959c56b545149323d75485c70
ureesoriano <oriolsoriano@gmail.com>
2016-04-30 22:17:06 +0200

    p1 solved

---
+++ p001.pl
@@ -0,0 +1,12 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+use 5.010;
+
+my $result = 0;
+$result += ($_ % 3 == 0 or $_ % 5 == 0) ? $_ : 0 foreach(1..999);
+
+#foreach (1..999) {
+#    $result += $_  if($_ % 3 == 0 or $_ % 5 == 0);
+#}
+say $result;
