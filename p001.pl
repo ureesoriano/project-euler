16b1ac38e6ed1a0b2956fb8f191e83363ecd5282
ureesoriano <oriolsoriano@gmail.com>
2016-04-30 22:18:41 +0200

    Ooops! left a commented block on prev commit

--- p001.pl
+++ p001.pl
@@ -4,9 +4,5 @@ use warnings;
 use 5.010;
 
 my $result = 0;
-$result += ($_ % 3 == 0 or $_ % 5 == 0) ? $_ : 0 foreach(1..999);
-
-#foreach (1..999) {
-#    $result += $_  if($_ % 3 == 0 or $_ % 5 == 0);
-#}
+$result += ($_ % 3 == 0 or $_ % 5 == 0) ? $_ : 0  foreach(1..999);
 say $result;
